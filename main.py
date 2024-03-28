b0c182caec806be5a85c5163a6d36278a6fb490a
frankdin <frankdin@yahoo.com>
2024-03-28 18:58:36 -0400

    Created a function, 'add_to_list' that takes as parameters the user's name and their bid and adds it to 'auction_list' as a dictionary.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-
 def clear():
     print("\033[2J\033[H", end="", flush=True)
+
+
 # 1. Create list that will contain dictionaries
 # 2. Ask for user's name and user bid
 # 3. Create function that takes user's name and bid as parameters
@@ -21,11 +22,13 @@ auction_list = [
     },
 ]
 
+
 # 2
 def get_bid_info():
-  name = input("What is your name?: ")
-  bid = int(input("What is your bid?: $"))
-  add_to_list(name, bid)
+    name = input("What is your name?: ")
+    bid = int(input("What is your bid?: $"))
+    add_to_list(name, bid)
+
 
 # #4
 # other_bidders = input("Are there any other bidders? Type 'yes' or 'no'.")
@@ -33,13 +36,15 @@ def get_bid_info():
 #   clear()
 #   get_bid_info()
 #
-# #3
-# def add_to_list(user_name, user_bid):
-#   bid_info = {
-#     user_name: user_bid
-#   }
-#   auction_list.append(bid_info)
-#
+# 3
+def add_to_list(user_name, user_bid):
+    bid_info = {
+        "name": user_name,
+        "bid": user_bid,
+    }
+    auction_list.append(bid_info)
+
+
 # #5
 # def get_highest_bid():
 #   highest_bid = auction_list[0]['bid']
@@ -47,7 +52,5 @@ def get_bid_info():
 #   for bid in auction_list:
 #
 # print(auction_list[0]['bid'])
-#print("text")
+# print("text")
 clear()
-
-
